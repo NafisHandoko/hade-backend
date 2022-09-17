98de5f2d0949c2f1a185de288c27b3d0e9e59e7a
Satrio <prassatrio8502@gmail.com>
2022-09-17 21:55:11 +0700

    Allow origin to public

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try {
   console.error(error);
 }
 
-app.use(cors({ credential: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credential: true, origin: '*' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
