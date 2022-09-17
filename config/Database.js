1a6413edbb19710712ca74bdff4e2a1878eb36ad
Satrio <prassatrio8502@gmail.com>
2022-09-17 10:29:25 +0700

    create login, register, and logout

--- config/Database.js
+++ config/Database.js
@@ -1,4 +1,7 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
